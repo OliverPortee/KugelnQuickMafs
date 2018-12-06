2835e3d743fa4f4c7b70f15fdb007aeb91e6f57e
Oliver Portee <oliver.portee@gmail.com>
2018-12-06 22:24:51 +0100

    added main structure with first geogebra scripts

---
+++ navigation-bar.js
@@ -0,0 +1,11 @@
+document.write(`
+ <div class="navbar">
+  
+    <a href="index.html"> Home </a> 
+    <a href="kugelgleichungen.html"> Kugelgleichungen </a> 
+    <a href="ebenen.html"> Ebenen </a> 
+    <a href="polar.html"> Polarebenen </a> 
+  
+</div>
+
+`);
